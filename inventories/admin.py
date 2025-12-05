1062d48b1423cba4e3b654bd3c2a7e7e2809fefc
Saray Rivas <saraysahar.r@gmail.com>
2025-12-04 23:43:18 -0500

    .

--- inventories/admin.py
+++ inventories/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from . models import  Product, Warehouse, Shelve, WarehouseCreation
+from . models import  Product, Warehouse, Shelve, WarehouseCreation, Inventory
 
-
+admin.site.register(Inventory)
 admin.site.register(Product)
 admin.site.register(Warehouse)
 admin.site.register(Shelve)
